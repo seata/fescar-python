8f08aa1ee56304364bae76bc1b172665f7aae4f3
jsbxyyx <jsbxyyx@163.com>
2021-08-24 15:56:38 +0800

    config

---
+++ seata/config/Configuration.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @author jsbxyyx
+# @since 1.0
+
+
+class Configuration:
+
+    def __init__(self):
+        # 1. 读取client.yml
+        # 2. 从client.yml中读取config节点配置
+        # 3. 从client.yml中读取registry节点配置
+
+        # 获取可用tc列表
+        # 1. 获取service.vgroup-mapping.${tx-service-group}对应的value
+        # 2. 获取service.${value}.grouplist对应tc节点ip:port信息
+        pass
